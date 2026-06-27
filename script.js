146b987a33e6ee0f73b3a4e324de12f76685d612
vitorbarb <242009963@aluno.unb.br>
2026-06-26 21:25:45 -0300

    Initializating brat-page with some clicking funcionalities

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("body").addEventListener("click", function(){
+    document.querySelector(".title").style.display = "none";
+    document.querySelector(".description").style.display = "none";
+})
